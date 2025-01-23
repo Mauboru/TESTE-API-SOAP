9c0b9b751b9e0516224f9e3916d4302a0a2b1512
Josue Henrique <josue21henrique@gmail.com>
2025-01-23 11:50:17 -0300

    corrigido com autenticacao no header

--- servidor/server.js
+++ servidor/server.js
@@ -1,14 +1,37 @@
+const express = require('express');
+const basicAuth = require('express-basic-auth');
 const soap = require('soap');
 const http = require('http');
 const fs = require('fs');
-const Cliente = require('./models/User'); 
+const Cliente = require('./models/User');
+const port = 8000;
+
+const app = express();
+
+const users = {
+  'admin': '123'
+};
 
 var myService = {
   MyService: {
     MyPort: {
-      getUsers: async function(args, callback) {
+      getUsers: async function(args, callback, headers) {
+        if (!headers || !headers.authentication || !headers.authentication.username || !headers.authentication.password) {
+          return callback({
+            faultcode: 'SOAP-ENV:Server',
+            faultstring: 'Missing authentication credentials',
+          });
+        }
+
+        if (headers.authentication.username !== 'admin' || headers.authentication.password !== '123') {
+          return callback({
+            faultcode: 'SOAP-ENV:Server',
+            faultstring: 'Invalid username or password',
+          });
+        }
+
         try {
-          const usuarios = await Cliente.findAll(); 
+          const usuarios = await Cliente.findAll();
           const userList = usuarios.map(user => ({
             CODIGO: user.CODIGO,
             COD_FUNCIONARIO: user.COD_FUNCIONARIO,
@@ -16,7 +39,7 @@ var myService = {
             SENHA: user.SENHA
           }));
 
-          callback({ users: { user: userList } }); 
+          callback({ users: { user: userList } });
         } catch (error) {
           console.error('Erro ao listar usuários:', error);
           callback({ users: [] }); 
@@ -40,8 +63,8 @@ var server = http.createServer(function(request, response) {
   }
 });
 
-server.listen(8000, () => {
-  console.log('SOAP server is running at http://localhost:8000');
-});
-
 soap.listen(server, '/soap', myService, xml);
+
+server.listen(port, () => {
+  console.log(`SOAP server is running at http://localhost:${port}`);
+});
